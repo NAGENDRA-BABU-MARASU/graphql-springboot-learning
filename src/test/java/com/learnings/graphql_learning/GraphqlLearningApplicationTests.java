9335e684650ec3403abacd1a47002a7ad72431c2
Nagendra <nagendrababu.marasu@gmail.com>
2024-08-16 23:15:52 +0530

    graphql springboot simple query with test

---
+++ src/test/java/com/learnings/graphql_learning/GraphqlLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.graphql_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
